63556f009c87e43c952d46f21caca6aa24a44dc9
Michael1182mitnick <56063336+Michael1182mitnick@users.noreply.github.com>
2024-06-20 17:24:30 -0800

    Create test-repo1.py

---
+++ test-repo1.py
@@ -0,0 +1,8 @@
+# Adding two integer numbers
+first = int(input("Enter your first integer num: "))
+second = int(input("Enter your second integer num: "))
+
+# two add first and second variables
+sum = first + second
+# to print and disply them in the output
+print("The SUM of two integers: " + str(sum))
